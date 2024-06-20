8689f795c2b4669c420912b2d20b81d833adf430
xecrzy <xyhchia7@outlook.com>
2024-06-20 16:14:54 +0800

    first commit

---
+++ src/views/news/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import News from './components/news.vue';
+import mianbaoxue from './components/mianbaoxue.vue';
+</script>
+
+<template>
+    <mianbaoxue></mianbaoxue>
+    <News></News>
+    
+    
+    <NewsDetail></NewsDetail>
+</template>
+
+<style scoped>
+
+</style>
